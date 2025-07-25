7597e48c20789cfc587a60cf3fdf77fd2b005103
DanielMP1995 <danielsantiagomeraparedes@hotmail.com.com>
2025-07-25 08:52:52 -0500

    segunda subida

--- src/app/pages/nosotros/nosotros.component.ts
+++ src/app/pages/nosotros/nosotros.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InfoNosotrosComponent } from "../../componentes/info-nosotros/info-nosotros.component";
+import { ComentariosComponent } from "../../componentes/comentarios/comentarios.component";
 
 @Component({
   selector: 'app-nosotros',
   standalone: true,
-  imports: [InfoNosotrosComponent],
+  imports: [InfoNosotrosComponent, ComentariosComponent],
   templateUrl: './nosotros.component.html',
   styleUrl: './nosotros.component.css'
 })
